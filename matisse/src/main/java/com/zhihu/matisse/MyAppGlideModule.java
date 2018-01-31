dacd6fc36ec54ec5f3a68e53eba146e9765a92c9
黄景强 <huangjingqiang@han-zi.cn>
2018-01-31 14:15:07 +0800

    替换glide版本为4.5，注入代码glideApp

---
+++ matisse/src/main/java/com/zhihu/matisse/MyAppGlideModule.java
@@ -0,0 +1,13 @@
+package com.zhihu.matisse;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Create by xjs
+ * _______date : 18/1/31
+ * _______description:
+ */
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
--- matisse/src/main/java/com/zhihu/matisse/engine/impl/GlideEngine.java
+++ matisse/src/main/java/com/zhihu/matisse/engine/impl/GlideEngine.java
@@ -20,8 +20,8 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
+import com.zhihu.matisse.GlideApp;
 import com.zhihu.matisse.engine.ImageEngine;
 
 /**
@@ -32,9 +32,9 @@ public class GlideEngine implements ImageEngine {
 
     @Override
     public void loadThumbnail(Context context, int resize, Drawable placeholder, ImageView imageView, Uri uri) {
-        Glide.with(context)
+        GlideApp.with(context)
                 .load(uri)
-                .asBitmap()  // some .jpeg files are actually gif
+               // .asBitmap()  // some .jpeg files are actually gif
                 .placeholder(placeholder)
                 .override(resize, resize)
                 .centerCrop()
@@ -44,9 +44,9 @@ public class GlideEngine implements ImageEngine {
     @Override
     public void loadGifThumbnail(Context context, int resize, Drawable placeholder, ImageView imageView,
                                  Uri uri) {
-        Glide.with(context)
+        GlideApp.with(context)
                 .load(uri)
-                .asBitmap()
+              //  .asBitmap()
                 .placeholder(placeholder)
                 .override(resize, resize)
                 .centerCrop()
@@ -55,7 +55,7 @@ public class GlideEngine implements ImageEngine {
 
     @Override
     public void loadImage(Context context, int resizeX, int resizeY, ImageView imageView, Uri uri) {
-        Glide.with(context)
+        GlideApp.with(context)
                 .load(uri)
                 .override(resizeX, resizeY)
                 .priority(Priority.HIGH)
@@ -64,9 +64,9 @@ public class GlideEngine implements ImageEngine {
 
     @Override
     public void loadGifImage(Context context, int resizeX, int resizeY, ImageView imageView, Uri uri) {
-        Glide.with(context)
+        GlideApp.with(context)
                 .load(uri)
-                .asGif()
+         //       .asGif()
                 .override(resizeX, resizeY)
                 .priority(Priority.HIGH)
                 .into(imageView);
